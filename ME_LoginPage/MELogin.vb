f30c1ed4f96d622c12783e930ed2cac74d3533e2
Josh <Josh@FUNGUSMCGOO>
2024-07-25 15:16:13 +0800

    Added detailed comments to explain code and parameters

--- ME_LoginPage/MELogin.vb
+++ ME_LoginPage/MELogin.vb
@@ -1,15 +1,17 @@
 ﻿Imports System.Data.SqlClient
 Imports Microsoft.SqlServer
 Public Class MELogin
-
-    '(LocalDb)\MSSQLLocalDB
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        'SQL Server Express Localdb connection string
         Dim conStr As String = "Server=(localdb)\MSSQLLocalDB;Integrated Security=true;Database=MONOLITHIC;"
+
+        'Error if attempting to login while username field is left empty
         If String.IsNullOrEmpty(txtboxUser.Text) Then
             MessageBox.Show("Please enter your username.")
             Return
         End If
 
+        'Error if attempting to login while password field is left empty
         If String.IsNullOrEmpty(txtboxPass.Text) Then
             MessageBox.Show("Please enter your password.")
             Return
@@ -21,10 +23,13 @@ Public Class MELogin
                 Dim query As String = "SELECT COUNT(*) FROM ME_users WHERE ME_username=@ME_username AND ME_password=@ME_password"
                 Using cmdSql As New SqlCommand(query, conSql)
 
+                    'Replace @ME_username and @ME_password with your databases column names
                     cmdSql.Parameters.AddWithValue("@ME_username", txtboxUser.Text)
                     cmdSql.Parameters.AddWithValue("@ME_password", txtboxPass.Text)
-                    Dim result As Integer = Convert.ToInt32(cmdSql.ExecuteScalar()) 'da fuq is dis??
+
+                    Dim result As Integer = Convert.ToInt32(cmdSql.ExecuteScalar())
                     If result > 0 Then
+                        'If user/pass matches, hide login and show main form
                         MessageBox.Show("Login Successful.")
                         MEmain.Show()
                         Me.Hide()
@@ -32,9 +37,8 @@ Public Class MELogin
                         MessageBox.Show("Invalid Username or Password.")
                     End If
                 End Using
-            Catch ex As SqlException '?? check for sql faiL?
+            Catch ex As SqlException 'Check for SQL failures- this is helpful for troubleshooting connectivity to database
                 MessageBox.Show("Error connecting to database: " & ex.Message)
-
             End Try
         End Using
     End Sub
